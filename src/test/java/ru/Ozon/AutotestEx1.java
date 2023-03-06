36d13f5462633af27fce5e3c503660e6eba9b679
Maryautotester <118917126+Maryautotester@users.noreply.github.com>
2023-03-06 13:10:14 +0300

    Updated

---
+++ src/test/java/ru/Ozon/AutotestEx1.java
@@ -0,0 +1,60 @@
+package ru.Ozon;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AutotestEx1 {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public JavascriptExecutor js;
+
+    @Before
+    public void SetUp() {
+        System.setProperty("webdriver.chrome.driver", "src/ChromeDriver/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--user-data-dir=C:\\Users\\test\\AppData\\Local\\Google\\Chrome\\User Data\\", "--profile-directory=Profile 2");
+        driver = new ChromeDriver();   // options
+
+        js = (JavascriptExecutor)driver;
+    }
+    @Test
+    public void autotestEx1() throws InterruptedException {
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+        FilterPage filterPage = PageFactory.initElements(driver, FilterPage.class);
+        CartPage cartPage = PageFactory.initElements(driver, CartPage.class);
+
+        homePage.open();
+        homePage.search();
+        filterPage.setAllFilters();
+        homePage.changeSorting();
+
+        int i = 0;
+        do {
+            js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+            WebElement next = driver.findElement(By.xpath("//div[contains(text(), 'Дальше')]"));
+            js.executeScript("arguments[0].scrollIntoView(true); window.scrollBy(0, -100);", next);
+            new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Дальше')]")));
+            next.click();
+            i++;
+            System.out.println(i);
+            if (driver.findElements(By.xpath("//span[contains(text(), 'Зонт Vogue')]")).size() > 0) {
+                break;
+            }
+        } while (i < 11);
+
+        homePage.chooseVogue();
+        homePage.addVogueToCart();
+
+        cartPage.Cart();
+    }
+}
